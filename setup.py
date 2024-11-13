9d1027f006ec6cffb8890d5e199e9808c0b5d06a
Baviskar Rohan Kantilal <rbavisakar9061@gmail.com>
2024-11-14 01:15:27 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    This function returns a list of requirements.
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements]  # Strips "\n" correctly
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='rohan',
+    author_email='rbaviskar9061@gmail.com',
+    packages=find_packages(),  # This will include all packages in the project directory
+    install_requires=get_requirements('requirements.txt')
+)
